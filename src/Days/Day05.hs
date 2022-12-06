27e6b0493e68ab5b22939446e4b63ce24788a9e6
astaugaard <64783657+astaugaard@users.noreply.github.com>
2022-12-06 16:47:31 -0500

    day 5 and 6

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,5 +1,5 @@
 module Days.Day05 (runDay) where
-
+    
 {- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
@@ -15,6 +15,10 @@ import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
 import Test.HUnit
+import Util.Util
+import Data.Char (isDigit)
+import Data.Functor (($>))
+import Control.Applicative ((<|>))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -22,24 +26,63 @@ runDay = R.runDay inputParser partA partB tests
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = Input <$> (makeStacks <$> readStacks <* stackNumbers <* string "\n\n") <*> sepBy instruction (char '\n')
+
+makeStacks :: [String] -> Vector String
+makeStacks = Vec.fromList . map (dropWhile (== ' '))
+
+readStacks :: Parser [String]
+readStacks = transpose <$> many1 readLine
+
+readLine :: Parser String
+readLine = sepBy box (char ' ') <* char '\n'
+
+stackNumbers :: Parser ()
+stackNumbers = many1 (satisfy (\c -> c == ' ' || isDigit c)) $> ()
+
+box :: Parser Char
+box = (char '[' *> anyChar <* char ']') <|> (string "   " $> ' ')
+
+instruction :: Parser Instr
+instruction = Instr <$> (string "move " *> (read <$> many1 digit)) <*> (string " from " *> (subtract 1 . read <$> many1 digit)) <*> (string " to " *> (subtract 1 . read <$> many1 digit))
 
 ------------ TYPES ------------
-type Input = Void
+data Input = Input (Vector String) [Instr] deriving (Show)
 
-type OutputA = Void
+data Instr = Instr {number :: Int, from :: Int, to :: Int} deriving (Show)
 
-type OutputB = Void
+type OutputA = String
+
+type OutputB = String
 
 ------------ PART A ------------
+moveN :: Int -> [a] -> [a] -> ([a],[a])
+moveN n a b = (rest,reverse taken ++ b)
+    where (taken,rest) = splitAt n a
+
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (Input start instrs) = map head $ Vec.toList $ foldl' (makeMove moveN) start instrs -- TLNGFGMFN
+
+makeMove :: (forall a. Int -> [a] -> [a] -> ([a],[a])) -> Vector String -> Instr -> Vector String
+makeMove moven v (Instr amount from to) = v Vec.// [(from,from'),(to,to')]
+    where (from',to') = moven amount (v Vec.! from) (v Vec.! to)
+
 
 ------------ PART B ------------
+
+moveN' :: Int -> [a] -> [a] -> ([a],[a])
+moveN' n a b = (rest,taken ++ b)
+    where (taken,rest) = splitAt n a
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (Input start instrs) = map head $ Vec.toList $ foldl' (makeMove moveN') start instrs
 
 
 ------------ Tests  ------------
+testInput = "    [D]\n[N] [C]\n[Z] [M] [P]\n 1   2   3\n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2"
+
+
 tests :: Test
-tests = TestList []
+tests = TestList ["partA test: " ~: TestCase (inputTest inputParser partA testInput "CMZ"),
+                  "partB test: " ~: TestCase (inputTest inputParser partB testInput "MCD")]
