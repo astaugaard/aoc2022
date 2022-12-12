f15ddd13ee0921e2b1ebcc2d3a2cb917d4c7b04e
astaugaard <64783657+astaugaard@users.noreply.github.com>
2022-12-12 17:24:00 -0500

    day 9 10 and 11

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -7,39 +9,125 @@ import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
+import Data.Array (Array)
+import qualified Data.Array as Array
 import qualified Util.Util as U
-
+import Lens.Micro.Platform
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (State)
+import Control.Monad.State
 import Data.Void
-import Test.HUnit
+import Test.HUnit hiding (State)
+import Control.Applicative (Alternative((<|>)))
 {- ORMOLU_ENABLE -}
 
-runDay :: R.Day
-runDay = R.runDay inputParser partA partB tests
+data CurrentState = CurrentState {_hloc :: (Int, Int), _tloc :: (Int, Int), _tailOn :: Set (Int, Int)}
+
+makeLenses ''CurrentState
+
+data CurrentState2 = CurrentState2 {_headL :: (Int, Int), _tailLocs :: [(Int, Int)], _beenOn :: Set (Int, Int)}
+
+makeLenses ''CurrentState2
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy row (char '\n')
+
+row :: Parser (Dir, Int)
+row = (,) <$> dir <*> (char ' ' *> (read <$> many1 digit))
+
+dir :: Parser Dir
+dir =
+  U <$ char 'U'
+    <|> Do <$ char 'D'
+    <|> L <$ char 'L'
+    <|> R <$ char 'R'
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Dir, Int)]
 
-type OutputA = Void
+data Dir = U | Do | L | R deriving (Show, Eq)
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+
+oneStep :: Dir -> State CurrentState ()
+oneStep dir = do
+  case dir of
+    U -> hloc . _2 += 1
+    Do -> hloc . _2 -= 1
+    L -> hloc . _1 -= 1
+    R -> hloc . _1 += 1
+  h <- use hloc
+  t <- use tloc
+  let t' = updateTail h t
+  tloc .= t'
+  updateSet
+
+updateSet :: State CurrentState ()
+updateSet = do
+  l <- use tloc
+  tailOn %= Set.insert l
+
+updateTail :: (Int, Int) -> (Int, Int) -> (Int, Int)
+updateTail h@(xh, yh) t@(xt, yt) = if abs (xt - xh) > 1 || abs (yt - yh) > 1 then moveTail h t else t
+
+moveTail :: (Int, Int) -> (Int, Int) -> (Int, Int)
+moveTail (xh, yh) (xt, yt) = (moveCord xh xt, moveCord yh yt)
+  where
+    moveCord h t
+      | t > h = t - 1
+      | t < h = t + 1
+      | otherwise = t
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA i = Set.size (resultState ^. tailOn)
+  where
+    moves = concatMap (uncurry (flip replicate)) i
+    resultState = execState (forM moves oneStep) $ CurrentState (0, 0) (0, 0) Set.empty
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
 
+oneStep' :: Dir -> State CurrentState2 ()
+oneStep' dir = do
+  case dir of
+    U -> headL . _2 += 1
+    Do -> headL . _2 -= 1
+    L -> headL . _1 -= 1
+    R -> headL . _1 += 1
+  h <- use headL
+  tail <- use tailLocs
+  let tail' = advanceTails h tail
+  tailLocs .= tail'
+  beenOn %= Set.insert (last tail') -- this could be faster if I made it so that it doesn't have to loop over the list again by
+                                    -- storing the tail elements in reverse and doing a fold instead of a unfold on the list
+                                    -- or I could have also used a vector or an array to store the tail elements and just indexed
+                                    -- to the end of it
+
+advanceTails :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+advanceTails h t = unfoldr go (h,t)
+    where go (h,[]) = Nothing
+          go (h,x:xs) = Just (nt,(nt,xs))
+            where nt = updateTail h x
+
+
+partB :: Input -> OutputB
+partB i = Set.size (resultState ^. beenOn)
+  where
+    moves = concatMap (uncurry (flip replicate)) i
+    resultState = execState (forM moves oneStep') $ CurrentState2 (0, 0) (replicate 9 (0, 0)) Set.empty
 
 ------------ Tests  ------------
+
+testI = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
+
+testI2 = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
+
 tests :: Test
-tests = TestList []
+tests = TestList [TestCase $ U.inputTest inputParser partA testI 13, TestCase $ U.inputTest inputParser partB testI2 36]
+
+runDay :: R.Day
+runDay = R.runDay inputParser partA partB tests
--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,5 +1,5 @@
 module Days.Day11 (runDay) where
-
+    
 {- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
@@ -14,7 +14,14 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
-import Test.HUnit
+import Test.HUnit hiding (State, test)
+import Data.Functor (($>))
+import Control.Applicative (Alternative((<|>)))
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Foldable (toList)
+import Control.Monad.State.Strict
+import Data.Ord
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -22,24 +29,96 @@ runDay = R.runDay inputParser partA partB tests
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy1 monkey (string "\n\n")
+
+pnumber :: Parser Int
+pnumber = read <$> many1 digit
+
+monkey :: Parser Monkey
+monkey =
+  Monkey 0
+    <$> (string "Monkey " *> pnumber <* string ":\n")
+    <*> (string "  Starting items: " *> (Seq.fromList <$> sepBy1 (toEnum <$> pnumber) (string ", ")) <* char '\n')
+    <*> (string "  Operation: new = " *> exprParse <* char '\n')
+    <*> (string "  Test: divisible by " *> (toEnum <$> pnumber) <* char '\n')
+    <*> (string "    If true: throw to monkey " *> pnumber <* char '\n')
+    <*> (string "    If false: throw to monkey " *> pnumber)
+
+exprParse :: Parser Expr
+exprParse = (\v o v' -> o v v') <$> (valueParse <* char ' ') <*> (operator <* char ' ') <*> valueParse
+
+operator :: Parser (Value -> Value -> Expr)
+operator = (char '+' $> Plus) <|> (char '*' $> Times)
+
+valueParse :: Parser Value
+valueParse = (string "old" $> OldValue) <|> (Number <$> (toEnum <$> pnumber))
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Monkey]
 
-type OutputA = Void
+data Monkey = Monkey {inspected :: Int, number :: Int, items :: Seq Int, operation :: Expr, test :: Int, trueCond :: Int, falseCond :: Int} deriving (Show)
 
-type OutputB = Void
+data Expr = Times Value Value | Plus Value Value deriving (Show)
+
+data Value = Number Int | OldValue deriving (Show)
+
+
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+
+runValue :: Int -> Value -> Int
+runValue n (Number i) = i
+runValue n OldValue = n
+
+runExpr :: Int -> Int -> Expr -> Int
+runExpr l n (Times va va') = (runValue n va * runValue n va') `mod` l
+runExpr l n (Plus va va') = (runValue n va + runValue n va') `mod` l
+
+runMonkey :: Int -> (Int -> Int) -> Monkey -> (Monkey, [(Int, Int)])
+runMonkey l worryEffect (Monkey ins i ns ex cond t f) = (Monkey (ins + length ns) i Seq.empty ex cond t f, map updatePos (toList ns))
+  where
+    updatePos :: Int -> (Int,Int)
+    updatePos v = (if v' `mod` cond == 0 then t else f, v')
+      where
+        v' = worryEffect (runExpr l v ex)
+
+runRound :: Int -> (Int -> Int) -> State (Vector Monkey) ()
+runRound l w =
+  do i <- gets length
+     forM_ [0..i-1] $ \ind ->
+       do m <- gets (Vec.! ind)
+          let (newMonk,throwList) = runMonkey l w m
+          modify $ \s -> Vec.accum (\m item -> m {items = items m Seq.|> item}) (s Vec.// [(ind,newMonk)]) throwList
+
+
+sortDes :: Ord a => [a] -> [a]
+sortDes = map getDown . sort . map Down
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA mons = a * b
+  where
+    vec = Vec.fromList mons
+    l = foldl1 lcm $ map test mons
+    final = execState (replicateM 20 (runRound l (`quot` 3))) vec
+    (a:b:_) = sortDes $ map inspected $ toList final
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
-
+partB mons = a * b
+  where
+    vec = Vec.fromList mons
+    final = execState (replicateM 10000 (runRound l id)) vec
+    l = foldl1 lcm $ map test mons
+    (a:b:_) = sortDes $ map inspected $ toList final
 
 ------------ Tests  ------------
+
+testInput = "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1\n"
+
 tests :: Test
-tests = TestList []
+tests = TestList [TestCase $ U.inputTest inputParser partA testInput 10605,
+                  TestCase $ U.inputTest inputParser partB testInput 2713310158]
